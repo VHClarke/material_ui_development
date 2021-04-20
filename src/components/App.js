ce9c0d8ae2f5c1aab70ba39c1d4fafccda93dafd
VHClarke <VanessaHClarke@gmail.com>
2021-04-20 09:12:10 -0700

    Working app w/ Hello string

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '../components/ui/Header'
 import {ThemeProvider } from '@material-ui/styles';
-
 import theme from './ui/Theme'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
     <Header/>
+    <h1>Hello</h1>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
 
 
 function ElevationScroll(props) {
@@ -16,14 +17,27 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarmargin:{
+    ...theme.mixins.toolbar
+  }
+}));
+
+
 export default function Header(props){
+  const classes = useStyles()
   return (
+    <React.Fragment>
     <ElevationScroll>
     <AppBar position="fixed">
     <Toolbar>
-    <Typography variant="h3">Arc Development</Typography>
+    <Typography variant="h3">
+    Arc Development
+    </Typography>
     </Toolbar>
     </AppBar>
     </ElevationScroll>
+    <div className={classes.toolbarmargin}/>
+    </React.Fragment>
   );
 }
